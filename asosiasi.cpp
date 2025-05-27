f4e934b500ea30637ea76da0dff26768376cc5d5
tegarrramadhaaan <anakrama2000@gmail.com>
2025-05-27 11:01:45 +0700

    Tambah relasi dokter dan pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -20,3 +20,26 @@ public:
     void tambahDokter(dokter *);
     void cetakDokter();
 };
+
+class dokter
+{
+    public:
+    string nama;
+    vector<pasien *> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama)
+    {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter()
+    {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+    void tambahPasien(pasien *);
+    void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter *pDokter)
+{
+    daftar_dokter.push_back(pDokter);
+}
