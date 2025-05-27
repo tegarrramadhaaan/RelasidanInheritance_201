3de8d72dc86a2d54d3be5689da0890fa435298bc
tegarrramadhaaan <anakrama2000@gmail.com>
2025-05-27 11:20:00 +0700

    Tambah relasi dokter dan pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -43,3 +43,57 @@ void pasien::tambahDokter(dokter *pDokter)
 {
     daftar_dokter.push_back(pDokter);
 }
+
+void pasien::cetakDokter()
+{
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto &a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+void dokter::tambahPasien(pasien *pPasien)
+{
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien()
+{
+    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto &a : daftar_pasien)
+    {
+        cout << a->nama << nama << "\n";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    dokter *varDokter1 = new dokter("dr.budi");
+    dokter *varDokter2 = new dokter("dr.tomo");
+    pasien *varPasien1 = new pasien("andi");
+    pasien *varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+
+    varPasien1->tambahDokter(varDokter1);
+    varPasien2->tambahDokter(varDokter1);
+    varPasien1->tambahDokter(varDokter2);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
